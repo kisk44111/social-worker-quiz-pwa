81947a3286f8e8ec91d13bb78ea322815ee0055c
kisk44111 <kisk44111@gmail.com>
2026-07-18 19:42:52 +0800

    Add AI 起承轉合 reference answers for 110 essays

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'social-worker-quiz-v2';
-const DATA_CACHE = 'social-worker-quiz-data-v2';
+const CACHE = 'social-worker-quiz-v3';
+const DATA_CACHE = 'social-worker-quiz-data-v3';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => ![CACHE,DATA_CACHE].includes(k)).map(k => caches.delete(k)))).then(() => self.clients.claim())));
